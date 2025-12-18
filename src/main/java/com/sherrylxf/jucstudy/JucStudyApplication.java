9c85b3e466da06da1b3246d5b0d5e695b3e49c72
sherrylxf <1298512407@qq.com>
2025-12-18 15:39:48 +0800

    2.线程的创建有三种方式：Thread、Runnable、Callable+FutureTask

--- src/main/java/com/sherrylxf/jucstudy/JucStudyApplication.java
+++ src/main/java/com/sherrylxf/jucstudy/JucStudyApplication.java
@@ -5,10 +5,13 @@ import com.sherrylxf.jucstudy.threadCreate.MyThread;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 @SpringBootApplication
 public class JucStudyApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         /*
         创建线程的方式
         1. 继承Thread类
@@ -26,6 +29,15 @@ public class JucStudyApplication {
         Thread myThread3 = new Thread(() -> {
             System.out.println("Runnable函数式接口，用Lambda表达式创建线程");
         });
+
+        FutureTask<Integer> futureTask = new FutureTask<>(() -> {
+            System.out.println("Callable函数式接口重写call，用Lambda表达式创建线程");
+            return 50;
+        });
+        Thread myThread4 = new Thread(futureTask);
+        myThread4.start();
+        Integer value = futureTask.get();
+        System.out.println("FutureTask + Callable 创建线程，并有线程的返回值： " + value);
 
         System.out.println("主线程");
 
